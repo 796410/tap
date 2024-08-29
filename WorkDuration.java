e4ace5a34192acaab86a3c585ed185348b8b7bcd
796410 <54370718+796410@users.noreply.github.com>
2024-08-29 18:08:15 +0530

    Added Work Duration POJO

---
+++ WorkDuration.java
@@ -0,0 +1,20 @@
+package com.practice.code;
+
+public class WorkDuration {
+	
+	int startTime;
+	int endTime;
+	public int getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(int startTime) {
+		this.startTime = startTime;
+	}
+	public int getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(int endTime) {
+		this.endTime = endTime;
+	}
+
+}
